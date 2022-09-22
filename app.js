fa746d901d6e79db0d795bf9bbe4e74a6047ae69
Todd Wolden <tcwolden@gmail.com>
2022-09-21 18:23:08 -0600

    Added JavaScript

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-let message = "Hi There! ";
-let usersName = prompt("What is your name?");
-console.log("Hi there!  " + usersName + " It is a wonderful day!");
-document.write(usersName)
+let userName = prompt("What is your name?");
+console.log(userName);
+document.write("Hi there,  " + userName + " have a wonderful day and thanks for visiting.")
+
+let number = 3;
+
+let answer = prompt("How many pictures of animals do think are on this page?");
+if (answer == number) {
+    alert("You are genius! That is correct.");
+   }   else if (answer == 2){
+       alert("Try again you are so close!");
+   }   else if(answer == 1){
+       alert("Did you scroll down the page? Please try again.");
+   }   else if(answer > 3){
+       alert("Have you been drinking? You are seeing double right now please try again later.")
+   }
+else {
+    alert("That is incorrect we are looking for a number.");
+}
+
